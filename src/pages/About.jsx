85066ee227679113fa4b5728ec7b82660c4d27f9
Ayoub MOSLIH <moslihayoub@gmail.com>
2026-07-23 14:33:50 +0100

    Fix parse error in About.jsx missing div

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -233,6 +233,7 @@ export default function About() {
                 </motion.div>
               </div>
             </motion.div>
+            </div>
           </section>
 
           {/* CLIENT MARQUEE */}
