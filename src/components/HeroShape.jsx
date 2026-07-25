347c4672f67f50b9bcaaada9db9e3aa824e5cfbb
Ayoub MOSLIH <moslihayoub@gmail.com>
2026-07-25 02:56:32 +0100

    fix: nettoyage import inutilise useThree dans HeroShape

--- src/components/HeroShape.jsx
+++ src/components/HeroShape.jsx
@@ -1,5 +1,5 @@
 import { useRef, useMemo } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { MeshTransmissionMaterial, Environment } from '@react-three/drei';
 import * as THREE from 'three';
 
